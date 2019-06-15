ceb51688c1539c5fdea2cf2489b5bc4aee1b37eb
qiulanbook <2549474324@qq.com>
2019-06-15 14:04:36 +0800

    xiu3

--- zy/js/article.js
+++ zy/js/article.js
@@ -2,21 +2,9 @@
 * @Author: Administrator
 * @Date:   2019-06-13 14:59:59
 * @Last Modified by:   Administrator
-* @Last Modified time: 2019-06-15 10:30:02
+* @Last Modified time: 2019-06-15 14:00:02
 */
 'use strict';
-// window.onload=function(){
-// 	var ofoot=document.getElementById('foot');
-// 	var ofootli=ofoot.getElementsByTagName('span');
-// 	var ofoot_s1=document.getElementById('foot_s1');
-// 	// var ofoot_s2=document.getElementById('foot_s1');
-// 	// var ofoot_s3=document.getElementById('foot_s1');
-// 	for (var i = 0; i < ofootli.length; i++) {
-// 	ofootli[i].onclick=function(){
-// 		this.style.color='#D81E06';
-// 	}
-// }
-// }
 $(function(){
 	// 星星
 	$(".foot_1").toggle(
@@ -27,12 +15,8 @@ $(function(){
   function () {
      $(".foot_s11").hide();
     $(".foot_s1").show();
-  }
-  
+  }  
 )
-
-
-
 	// 点赞
 	$(".foot_3").toggle(
 		function () {
@@ -44,41 +28,6 @@ $(function(){
      $(".foot_s33").hide();
     $(".foot_s3").show();
     $('.mix').hide();
-  }
-  
+  } 
 )
-	$(".foot_3").click(function() {
-		
-	});
-
-
-	// 转发
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
--- zy/js/index.js
+++
@@ -1,8 +0,0 @@
-/* 
-* @Author: Administrator
-* @Date:   2019-06-15 10:40:26
-* @Last Modified by:   Administrator
-* @Last Modified time: 2019-06-15 10:40:57
-*/
-
-'use strict';
